063f4d1af61b23f84e53aac71862469737209c00
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-01-30 13:50:10 -0400

    feat: :sparkles: add EnableSpringDataWebSupport stop warn in console

--- src/main/java/com/vinicius_lima/comanda_flash/ComandaFlashApplication.java
+++ src/main/java/com/vinicius_lima/comanda_flash/ComandaFlashApplication.java
@@ -2,8 +2,10 @@ package com.vinicius_lima.comanda_flash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ComandaFlashApplication {
 
     public static void main(String[] args) {

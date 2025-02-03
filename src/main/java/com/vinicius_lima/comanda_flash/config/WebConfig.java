df7d308d626a4e128c0a37da84567adf4205d3de
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-02-03 19:54:30 -0400

    feat: :sparkles: adjust config cors

--- src/main/java/com/vinicius_lima/comanda_flash/config/WebConfig.java
+++ src/main/java/com/vinicius_lima/comanda_flash/config/WebConfig.java
@@ -1,24 +1,15 @@
 package com.vinicius_lima.comanda_flash.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        UrlPathHelper urlPathHelper = new UrlPathHelper();
-        urlPathHelper.setRemoveSemicolonContent(false);
-        configurer.setUrlPathHelper(urlPathHelper);
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .addResourceLocations("classpath:/public/");
-    }
 }

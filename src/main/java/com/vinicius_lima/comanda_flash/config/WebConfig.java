e2e4922780c8e2ce455fc2f642221b2a63e99d93
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-02-04 09:06:22 -0400

    feat: :sparkles: adjust cors configurations

--- src/main/java/com/vinicius_lima/comanda_flash/config/WebConfig.java
+++ src/main/java/com/vinicius_lima/comanda_flash/config/WebConfig.java
@@ -1,19 +1,46 @@
 package com.vinicius_lima.comanda_flash.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://comanda-flash-production.up.railway.app", "https://comanda-flash-production.up.railway.app")
-                .allowedHeaders("*")
+        registry.addMapping("/swagger-ui/**")
+                .allowedOrigins("*")
                 .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+        registry.addMapping("/api-docs/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        String currentUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+
+        config.setAllowedOrigins(Arrays.asList(currentUrl));
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
+

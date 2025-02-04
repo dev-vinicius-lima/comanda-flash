39d08f4489fcbc47ef4df0828a05e5662b893a23
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-02-04 09:39:06 -0400

    feat: :sparkles: add springdoc.swagger-ui.protocol=https

--- src/main/java/com/vinicius_lima/comanda_flash/config/SwaggerConfig.java
+++ src/main/java/com/vinicius_lima/comanda_flash/config/SwaggerConfig.java
@@ -7,19 +7,22 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI swaggerApi() {
-        return new OpenAPI().info(new Info()
-                .title("Comanda Flash API")
-                .version("v1.0")
-                .description("API para gerenciamento de pedidos")
-                .contact(new Contact()
-                        .name("Vinicius Lima")
-                        .email("viniciuslimaes@hotmail.com")));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Comanda Flash API")
+                        .version("v1.0")
+                        .description("API para gerenciamento de pedidos")
+                        .contact(new Contact()
+                                .name("Vinicius Lima")
+                                .email("viniciuslimaes@hotmail.com")));
     }
 
 }
